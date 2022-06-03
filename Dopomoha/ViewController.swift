a184f12bfb00f104a1373ebefad20a0667429101
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-06-03 22:34:47 +0300

    Initial Commit

---
+++ Dopomoha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dopomoha
+//
+//  Created by Вячеслав Гудзенко on 03.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
