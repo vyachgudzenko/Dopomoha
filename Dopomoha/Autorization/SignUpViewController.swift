32c60b3064a83ea3aeb46b934e607c7a9d3cbfbb
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-06-08 17:21:49 +0300

    Added view controller for  Autorization

---
+++ Dopomoha/Autorization/SignUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpViewController.swift
+//  Dopomoha
+//
+//  Created by Вячеслав Гудзенко on 08.06.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButton()
+        setupTextField()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonPressed(_ sender:UIButton){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let specifiedController = storyboard.instantiateViewController(withIdentifier: "TabbarController")
+        navigationController?.pushViewController(specifiedController, animated: true)
+    }
+    
+    private func setupButton(){
+        let color = UIColor(red: 0.992, green: 0.918, blue: 0.255, alpha: 1)
+        loginButton.layer.borderColor = color.cgColor
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = 18
+    }
+    
+    private func setupTextField(){
+        let color = UIColor(red: 0.012, green: 0.667, blue: 0.965, alpha: 1)
+        phoneTextField.layer.borderColor = color.cgColor
+        phoneTextField.layer.borderWidth = 1
+    }
+}
---
+++ Dopomoha/Autorization/StartViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StartViewController.swift
+//  Dopomoha
+//
+//  Created by Вячеслав Гудзенко on 08.06.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    
+    @IBAction func buttonPressed(_ sender:UIButton){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let specifiedController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController")
+        navigationController?.pushViewController(specifiedController, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
